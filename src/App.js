a6696bc7f5d826a4f82183c951059040db6b3e8d
Matt Gethers <gethermc@gmail.com>
2021-12-18 14:59:27 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+//import logo from './logo.svg';
+import './App.css';
+//import Day5 from './sketches/Day5';
+import Day6 from './sketches/Day6';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Day6/>
+        <p>
+          hello Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
